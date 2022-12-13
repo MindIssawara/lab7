47b197298d150b7162fdce796060661fc9d19c8b
MindIssawara <kmind46@gmail.com>
2022-12-13 15:41:36 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 
 using namespace std;
-
+int  adiff(int a1,int a2){
+    int re;
+    a1= a1%360;
+    a2= a2%360;
+    if(a1>a2){
+        re = a1-a2;
+    } else{
+        re = a2-a1;
+    }
+    if(re>180){
+        return 360-re;
+    } else{
+        return re;
+    }
+}
 
 int main(){
   cout << adiff(180,270);
